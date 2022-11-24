5113d6f6621fada0f04602c1512a4706d4efcea0
_Pegasus8_ <v.malafronte7@studenti.unisa.it>
2022-11-24 13:00:04 +0100

    add tweet and concorrenti

---
+++ Project/concorrenti.py
@@ -0,0 +1,9 @@
+import ConcorrenteGf
+
+#andiamo a registrare tutti concorrenti del GF Vip prendendo i nomi da un file
+file_name = "../concorrentiGF.csv"
+
+conc_GF = []
+
+#nel for in cui si assegnano nome e cognome;
+# conc_GF.append(ConcorrenteGF(nome, cognome))
