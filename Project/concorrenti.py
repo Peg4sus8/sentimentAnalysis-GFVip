68a824a6667125480379c51609070a5a091e86e8
_Pegasus8_ <v.malafronte7@studenti.unisa.it>
2022-11-25 17:11:09 +0100

    concorrenti

--- Project/concorrenti.py
+++ Project/concorrenti.py
@@ -1,9 +1,20 @@
 import ConcorrenteGf
+import numpy
+import pandas
 
-#andiamo a registrare tutti concorrenti del GF Vip prendendo i nomi da un file
-file_name = "../concorrentiGF.csv"
+#andiamo a registrare tutti concorrenti del GF Vip prendendo i nomi dal file
+inputFile = "../concorrentiGF.csv"
+df = pandas.read_csv(inputFile, header = 0, delimiter = ";")
+
+#si inseriscono le colonne del file in un oggetto numpy
+#si possono inserire anche alcune colonne solo
+n = df.to_numpy()
 
 conc_GF = []
+
+for i in range(n.shape[0]):
+    provv = ConcorrenteGf(n[i, 0], n[i, 1])
+    conc_GF.append(provv)
 
 #nel for in cui si assegnano nome e cognome;
 # conc_GF.append(ConcorrenteGF(nome, cognome))
