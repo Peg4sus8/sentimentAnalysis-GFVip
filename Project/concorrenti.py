0ba14a2cbcdce5d652c857b03cf3df59774a97a1
_Pegasus8_ <v.malafronte7@studenti.unisa.it>
2022-11-29 11:46:51 +0100

    fix

--- Project/concorrenti.py
+++ Project/concorrenti.py
@@ -1,12 +1,12 @@
 from ConcorrenteGf import *
 import pandas
 
-#andiamo a registrare tutti concorrenti del GF Vip prendendo i nomi dal file
+# andiamo a registrare tutti concorrenti del GF Vip prendendo i nomi dal file
 inputFile = "sentimentAnalysis-GFVip/concorrentiGF.csv"
-df = pandas.read_csv(inputFile, header = 0, delimiter = ";")
+df = pandas.read_csv(inputFile, header=0, delimiter=";")
 
-#si inseriscono le colonne del file in un oggetto numpy
-#si possono inserire anche alcune colonne solo
+# si inseriscono le colonne del file in un oggetto numpy
+# si possono inserire anche alcune colonne solo
 
 n = df.to_numpy()
 concGF = []
@@ -16,8 +16,6 @@ for i in range(n.shape[0]):
     concGF.append(provv)
 
 # stampa concorrenti
-"""
 print("----------CONCORRENTI----------\n")
 for i in range(len(concGF)):
     print("--", concGF[i].getName(), concGF[i].getCognome())
-"""
